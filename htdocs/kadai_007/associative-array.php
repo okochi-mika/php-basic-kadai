4e310887c0be2d2b9999b6ab3b78b90f535f274f
okochi-mika <hiyoko_m@icloud.com>
2025-04-08 23:15:31 +0900

    コードを修正

--- htdocs/kadai_007/associative-array.php
+++ htdocs/kadai_007/associative-array.php
@@ -1,7 +1,7 @@
 <?php
 
  // 連想配列に値を代入する
-$personal_data = ['name' => 'oroon', 'price' => 200, 'weight' => '男性'];
+$personal_data = ['name' => 'oroon', 'price' => 200, 'weight' => '160'];
 
 // 連想配列の値を出力する
 print_r($personal_data);
