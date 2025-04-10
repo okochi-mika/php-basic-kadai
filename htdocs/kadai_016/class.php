9c32a52580ab467f87b2a0395e6510fc2c9e6547
okochi-mika <hiyoko_m@icloud.com>
2025-04-10 18:12:09 +0900

    ファイルを追加

---
+++ htdocs/kadai_016/class.php
@@ -0,0 +1,58 @@
+<?php
+class Food {
+    // プロパティを定義する
+    private $name;
+    private $price;
+
+    // コンストラクタを定義する
+    public function __construct($name, $price) {
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    // priceを出力するメソッド
+    public function show_price() {
+        return $this->price;
+    }
+}
+?>
+
+<?php
+class Animal {
+    // プロパティを定義する
+    private $name;
+    private $height;
+    private $weight;
+
+    // コンストラクタを定義する
+    public function __construct($name, $height, $weight) {
+        $this->name = $name;
+        $this->height = $height;
+        $this->weight = $weight;
+    }
+
+    // heightを出力するメソッド
+    public function show_height() {
+        return $this->height;
+    }
+}
+?>
+
+<?php
+// Foodインスタンスを作成
+$food = new Food();
+
+// Animalインスタンスを作成
+$animal = new Animal();
+
+// インスタンスを出力する
+echo "Food: " . $food->show_price(); // Foodクラスのpriceを出力
+echo "Animal: " . $animal->show_height(); // Animalクラスのheightを出力
+
+// print_rを使ってインスタンスのプロパティを表示
+echo "\nFood Object: ";
+print_r($food);
+
+echo "\nAnimal Object: ";
+print_r($animal);
+?>
